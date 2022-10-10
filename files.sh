531b1968a7b0a371cfab4ce48cc6d89f09c7b65f
unknown <myapps255@gmail.com>
2022-10-10 08:47:42 -0500

    initial commit

---
+++ files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STATUS_FILE=last.txt
+name="kevin"
+
+if [ -f "$STATUS_FILE" ]; then
+    num=$(cat $STATUS_FILE)
+fi
+
+for i in {1..25}
+do
+    fn=$(expr $num + $i)
+    file="${name}-${fn}"
+    touch $fn.txt
+done
+
+echo $fn > $STATUS_FILE
